741dcdda83e3e5b15530105bebfc7a08b7d03ce0
Alfred <alfred@Alfreds-MacBook.local>
2024-07-12 15:32:17 +0200

    added swap functionality and buttons to join and leave

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import datetime
+import json
 
 app = Flask(__name__)
 
@@ -7,8 +8,10 @@ queue = []
 enqueued_users = {}
 
 @app.route('/qjoin', methods=['POST'])
-def qjoin():
-    user_id = request.form['user_id']
+def qjoin_endpoint():
+    return qjoin(request.form['user_id'])
+
+def qjoin(user_id):
     message = f"{make_tag(user_id)} is already in the queue (on position {get_queue_position_by_id(user_id)})"
     
     if user_id not in [id for id in queue]:
@@ -19,8 +22,10 @@ def qjoin():
     return to_channel_response(message + f" \n{prettyCurrentQueue()}")
 
 @app.route('/qleave', methods=['POST'])
-def qleave():
-    user_id = request.form['user_id']
+def qleave_endpoint():
+    return qleave(request.form['user_id'])
+
+def qleave(user_id):
     message = f"{make_tag(user_id)} is not in the queue"
     
     global queue
@@ -31,17 +36,19 @@ def qleave():
     return to_channel_response(message)
 
 @app.route('/qshow', methods=['POST'])
-def show_queue():
+def show_queue_endpoint():
     return to_channel_response(prettyCurrentQueue())
 
 @app.route('/qswap', methods=['POST'])
-def qswap():
-    user_id = request.form['user_id']
+def qswap_endpoint():
+    
+    return qswap(request.form['user_id'], request.form['text'])
+def qswap(user_id, swap_with):
     message = f"Invalid swap requested."
 
     try:
         current_pos = get_queue_position_by_id(user_id)-1
-        swap_with_pos = int(request.form['text']) - 1 # crashes if invalid int
+        swap_with_pos = int(swap_with) - 1 # crashes if invalid int
 
         queue[current_pos] = queue[swap_with_pos]
         queue[swap_with_pos] = user_id
@@ -49,6 +56,17 @@ def qswap():
         message = f"{user_id} swapped places with {queue[current_pos]}\n{prettyCurrentQueue()}."
     finally:
         return to_channel_response(message)
+
+@app.route('/buttonproxy', methods=['POST'])
+def route_to_action():
+    payload = json.loads(request.form['payload'])
+    action = payload['actions'][0]['name']
+    user_id = payload['user']['id']
+
+    if action == "qjoin":
+        return qjoin(user_id)
+    if action == "qleave":
+        return qleave(user_id)
 
 def prettyCurrentQueue():
     if len(queue) > 0:
@@ -73,7 +91,30 @@ def simplify_timestamp(timestamp):
 def to_channel_response(message):
     return jsonify({
             "response_type": "in_channel",
-            "text": f"{message}"
+            "text": f"{message}",
+            "attachments": [
+            {
+                "text": "Actions:",
+                "fallback": "You are unable to manage the queue",
+                "callback_id": "queue_actions",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "qjoin",
+                        "text": "Join",
+                        "type": "button",
+                        "value": "join"
+                    },
+                    {
+                        "name": "qleave",
+                        "text": "Leave",
+                        "type": "button",
+                        "value": "leave"
+                    }
+                ]
+            }
+        ]
         })
 
 if __name__ == '__main__':
